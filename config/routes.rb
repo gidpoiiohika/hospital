3c8a544ffddddd31a2f6d9679f7c3ec56b2cdbdf
Yaroslav <adacer73044210@gmail.com>
2022-09-08 19:18:11 +0300

    add routes and edit package

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  resources :recommendations
-  resources :appointments
-  resources :categories
-  get 'pages/home'
-  get 'pages/profile_doctor'
-  get 'pages/profile_patient'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :doctors
-  devise_for :patients
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :doctors, controllers: { sessions: 'sessions' }
+  devise_for :patients, controllers: { sessions: 'sessions' }
+
+  resources :categories, only: :index
+  resources :appointments, only: [:index, :create]
+  resources :recommendations, only: [:index, :new, :create]
+  
+  root "pages#home"
+  
+  get '/page_doctor',         to: 'pages#page_doctor'
+  get '/profile_doctor/:id',  to: 'pages#profile_doctor',  as: 'profile_doctor'
+  get '/profile_patient/:id', to: 'pages#profile_patient', as: 'profile_patient'
 end
