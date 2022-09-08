da3a3ef309ad0f2b006ef05da6fdf0da937328ce
Yaroslav <adacer73044210@gmail.com>
2022-09-08 20:26:27 +0300

    add image to category and add setting amazon s3

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register Category do
 
-  permit_params :name, doctor_ids: []
+  permit_params :name, :image, doctor_ids: []
 
   index do
     selectable_column
     id_column
     column :name
     column :doctor_ids
+    column :image do |m|
+      image_tag m.image.url if m.image.present?
+    end
     actions
   end
 
@@ -15,6 +18,9 @@ ActiveAdmin.register Category do
       row :name
       row :doctor_ids
       row :updated_at
+      row :image do |ad|
+        image_tag ad.image.url if ad.image.present?
+      end
     end
   end
 
@@ -22,6 +28,7 @@ ActiveAdmin.register Category do
     f.inputs do
       f.input :name
       f.input :doctors, as: :select, multiple: true, collection: Doctor.all.collect {|product| [product.first_name, product.id]}
+      f.input :image, as: :file
     end
     f.actions
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :doctors
   validates :name, presence: true, uniqueness: {scope: :name}
+  has_one_attached :image
 end
