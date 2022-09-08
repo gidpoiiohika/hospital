5ad4f8d5a9f7cd5ce387720c2bff34e72b9e02ef
Yaroslav <adacer73044210@gmail.com>
2022-09-08 20:43:44 +0300

    hotfix

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,10 @@
 module AppointmentsHelper
+  def status_color(appointment)
+    case appointment.status
+    when 'open'
+      'text-danger'
+    when 'closed'
+      'text-success'
+    end
+  end
 end
