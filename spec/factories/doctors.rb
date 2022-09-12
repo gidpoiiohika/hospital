94ff393c465412af7ba99cd23f2a48efd7de88cb
Yaroslav <adacer73044210@gmail.com>
2022-09-12 16:41:04 +0300

    hotfix

--- spec/factories/doctors.rb
+++ spec/factories/doctors.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :doctor do
-    first_name { "DoctorFirstName" }
-    last_name { "DoctorLastName" }
-    phone_number { "787898" }
+    sequence(:first_name) { |n| "Doctor-#{n}" }
+    sequence(:last_name)  { |n| "Doctor-#{n}" }
+    sequence(:phone_number) { |n| "+1555#{n.to_s.rjust(7, '0')}" }
     email { "doctodEmail@gmail.com" }
     password { 123456 }
     password_confirmation { 123456 }
